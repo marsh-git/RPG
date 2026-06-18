3fcb51174f885bf7205aca93111908b5ce320d9f
marsh <f.koji5654@gmail.com>
2026-06-18 14:41:41 +0900

    イカサマダイス作成中

--- Assets/!TeamMember/Marsh/Src/Dice/DiceManager.cs
+++ Assets/!TeamMember/Marsh/Src/Dice/DiceManager.cs
@@ -2,26 +2,27 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class DiceFace : MonoBehaviour {
-    public int faceValue; // この面の数字（1〜6）
-}
-
-
 public class DiceManager : MonoBehaviour {
     [SerializeField] private Dice dicePrefab;
     [SerializeField] private Transform spawnPoint;
 
     private List<Dice> dices = new List<Dice>();
 
-    public IEnumerator RollDice(int count, System.Action<Dictionary<int, int>, int> onComplete) {
+    void Update() {
+        // 左クリックでサイコロを振る
+        if (Input.GetMouseButtonDown(0)) {
+            StartCoroutine(RollDice(1)); // 1個だけ投げる例
+        }
+    }
+
+    public IEnumerator RollDice(int count) {
         dices.Clear();
 
-        // サイコロ生成 & 投げる
         for (int i = 0; i < count; i++) {
             Dice d = Instantiate(dicePrefab, spawnPoint.position, Random.rotation);
             Rigidbody rb = d.GetComponent<Rigidbody>();
 
-            // DEMEO風：ランダム方向に投げる
+            // 投げる力
             Vector3 force = new Vector3(
                 Random.Range(-2f, 2f),
                 5f,
@@ -37,18 +38,9 @@ public class DiceManager : MonoBehaviour {
         // 全サイコロが止まるまで待つ
         yield return new WaitUntil(() => dices.TrueForAll(d => d.IsStopped()));
 
-        // 出目集計
-        Dictionary<int, int> counts = new Dictionary<int, int>();
-        int total = 0;
-
+        // 結果表示
         foreach (var d in dices) {
-            int r = d.GetResult();
-            total += r;
-
-            if (!counts.ContainsKey(r)) counts[r] = 0;
-            counts[r]++;
+            Debug.Log("出目: " + d.GetResult());
         }
-
-        onComplete?.Invoke(counts, total);
     }
 }

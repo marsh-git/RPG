d7e58eb98e965151c3acd3da5625d0bbb5390292
kuroneko1318 <nekkokuro5@gmail.com>
2026-06-19 10:39:52 +0900

    キャラクターベース追加

---
+++ Assets/!TeamMember/Neko/Src/Character/CharacterBase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+
+    //  ステータス
+    private int hp;
+    private int attack;
+    private int defense;
+    private int luck;
+
+
+}

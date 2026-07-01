c00fd1dc12d7508a089bc8e29152f68114e22151
kuroneko1318 <nekkokuro5@gmail.com>
2026-07-01 15:17:53 +0900

    プレイヤー経験値システム

---
+++ Assets/!TeamMember/Neko/Src/Character/JobData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/JobData")]
+public class JobData : ScriptableObject
+{
+
+    [Header("Jobのステータス")]
+    [SerializeField] public int maxHp;
+    [SerializeField] public int attack;
+    [SerializeField] public int defense;
+    [SerializeField] public int luck;
+
+    //  アクションのデータ配列
+
+    //  初期アクションデータ
+
+}
---
+++ Assets/!TeamMember/Neko/Src/Character/PlayerBase.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : CharacterBase
+{
+
+    //  経験値変数
+    private int exp = 0;
+    private int lv = 1;
+    private int needExp;
+    private readonly int baseNeedExp = 50;
+    private float needExpRatio = 1.5f;
+
+    //  外部参照用
+    public int Exp => exp;
+    public int Lv => lv;
+    public int NeedExp => needExp;
+
+    //  職業
+    [SerializeField] private JobData jobData = null;
+    private JobManager jobManager = null;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        jobManager = JobManager.instance;
+        needExp = baseNeedExp;
+
+        //  初期職業がnullなら初期職業に変更する
+        //if (jobData == null) SetJob(jobManager.START_JOB);
+    }
+
+    private void Update()
+    {
+        // 左クリックで経験値取得(デバッグ用)
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetExp(10);
+            Debug.Log("現在のexp = " + Exp);
+            Debug.Log("現在のlv = " + Lv);
+            Debug.Log("次の経験値まで = " + NeedExp);
+        }
+    }
+
+    /// <summary>
+    /// 敵から得れる経験値を引数に呼び出す
+    /// </summary>
+    /// <param name="getExp"></param>
+    public void GetExp(int getExp)
+    {
+        exp += getExp;
+
+        while(exp >= needExp)
+        {
+            Levelup();
+        }
+    }
+
+    /// <summary>
+    /// レベルアップ処理
+    /// </summary>
+    private void Levelup()
+    {
+        exp -= needExp;
+        //  万が一経験値が0を下回ったら
+        if(exp < 0) exp = 0;
+
+        lv += 1;
+        //  小数点以下切り捨て
+        needExp = Mathf.FloorToInt(baseNeedExp * Mathf.Pow(lv, needExpRatio));
+    }
+
+    /// <summary>
+    /// Jobをプレイヤーにセットする
+    /// </summary>
+    /// <param name="jobNum"></param>
+    private void SetJob(int jobNum)
+    {
+        //jobData = jobManager.GetJobData(jobNum);
+
+        //  職業がnullなら初期職業に変更する
+        if (jobData == null) SetJob(jobManager.START_JOB);
+
+        SetStatus(jobData);
+    }
+
+    /// <summary>
+    /// ステータスをセットする
+    /// </summary>
+    private void SetStatus(JobData jobData)
+    {
+        maxHp = jobData.maxHp;
+        hp = maxHp;
+        attack = jobData.attack;
+        defense = jobData.defense;
+        luck = jobData.luck;
+    }
+
+}

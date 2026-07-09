b18fb8dd6eed2338cc5c9ea0d154a2bfe204739a
kuroneko1318 <nekkokuro5@gmail.com>
2026-07-09 14:37:23 +0900

    イベントまだまだ作ってる

--- Assets/!TeamMember/Neko/Src/Event/EventDataBase.cs
+++ Assets/!TeamMember/Neko/Src/Event/EventDataBase.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class EventDataBase : ScriptableObject
 {
@@ -14,14 +16,42 @@ public abstract class EventDataBase : ScriptableObject
     [Header("イベント画像")]
     public Sprite eventImage;
 
+    //  子オブジェクトのUI配置（固定）
+    private readonly int EVENTUI_IMAGE = 1;
+    private readonly int EVENTUI_DESCRIPTION = 2;
+
     /// <summary>
     /// イベント開始関数
     /// </summary>
     public abstract void StartEvent();
 
-    protected virtual void SetEventUI()
+    /// <summary>
+    /// UIをセットする
+    /// </summary>
+    /// <param name="eventUI"></param>
+    public virtual void SetEventUI(Canvas eventUI)
     {
+        Transform uiParent = eventUI.transform;
+        var uiChildren = GetChildren(uiParent);
+        uiChildren[EVENTUI_IMAGE].GetComponent<Image>().sprite = eventImage;
+        uiChildren[EVENTUI_DESCRIPTION].GetComponent<TextMeshProUGUI>().text = eventDescription;
+    }
 
+    /// <summary>
+    /// 子オブジェクトを取得する
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <returns></returns>
+    private Transform[] GetChildren(Transform parent)
+    {
+        var children = new Transform[parent.childCount];
+
+        for (var i = 0; i < children.Length; i++)
+        {
+            children[i] = parent.GetChild(i);
+        }
+
+        return children;
     }
 
 }
--- Assets/!TeamMember/Neko/Src/Event/EventManager.cs
+++ Assets/!TeamMember/Neko/Src/Event/EventManager.cs
@@ -7,6 +7,9 @@ public class EventManager : MonoBehaviour
 {
     public static EventManager instance { get; private set; }
 
+    [Header("イベントUI")]
+    [SerializeField] private Canvas eventUI;
+
     [Header("ゲーム内で使用するイベントデータの配列")]
     [SerializeField] private EventDataBase[] eventDatas;
 
@@ -15,13 +18,35 @@ public class EventManager : MonoBehaviour
         instance = this;
     }
 
+    private void Start()
+    {
+        if(eventUI != null) eventUI.gameObject.SetActive(false);
+    }
+
     /// <summary>
     /// 任意のイベント開始
     /// </summary>
     /// <param name="eventNum"></param>
     public void StartEvent(int eventNum)
     {
+        SetEventUI(eventNum);
+
         eventDatas[eventNum].StartEvent();
+    }
+
+    /// <summary>
+    /// イベントUIをセットする
+    /// </summary>
+    private void SetEventUI(int eventNum)
+    {
+        if (eventUI == null)
+        {
+            Debug.LogWarning("eventUIが取得できませんでした");
+            return;
+        }
+        eventUI.gameObject.SetActive(true);
+
+        eventDatas[eventNum].SetEventUI(eventUI);
     }
 
 }

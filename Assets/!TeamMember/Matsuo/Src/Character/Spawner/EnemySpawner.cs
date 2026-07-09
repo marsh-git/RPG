61e015351ed3220247fff02e1c28fc5ae56daa9c
Yu-Matuo <mcd2476068@stu.o-hara.ac.jp>
2026-07-09 09:58:30 +0900

    EnemySpawner

--- Assets/!TeamMember/Matsuo/Src/Character/Spawner/EnemySpawner.cs
+++ Assets/!TeamMember/Matsuo/Src/Character/Spawner/EnemySpawner.cs
@@ -1,45 +1,52 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     // 敵プレハブ
     [SerializeField]
-    private EnemyBase enemyPrefab;
-
-    // キャラクター管理クラス
-    [SerializeField]
-    private CharacterManager characterManager;
+    private EnemyBase enemyPrefab = null;
 
     /// <summary>
-    /// 指定したタイルに敵を生成する
+    /// 候補タイルからランダムな位置へ敵を生成する
     /// </summary>
-    /// <param name="spawnTile">生成先のタイル</param>
-    /// <returns>生成した敵</returns>
-    public EnemyBase Spawn(int spawnTileID)
+    /// <param name="candidateTiles">生成候補タイル</param>
+    /// <param name="spawnCount">生成する敵の数</param>
+    public void SpawnEnemy(List<HexTileObject> candidateTiles, int spawnCount)
     {
-        // 敵プレハブが設定されているか確認する
-        if (enemyPrefab == null)
+        // 候補タイルまたはプレハブが存在しない場合は処理しない
+        if (candidateTiles == null || candidateTiles.Count == 0 || enemyPrefab == null)
         {
-            return null;
+            return;
         }
 
-        // CharacterManagerが設定されているか確認する
-        if (characterManager == null)
+        // 同じタイルへ生成しないよう候補リストをコピーする
+        List<HexTileObject> spawnTiles = new List<HexTileObject>(candidateTiles);
+
+        // 指定数だけ敵を生成する
+        for (int i = 0; i < spawnCount; i++)
         {
-            return null;
+            // 候補タイルがなくなったら終了する
+            if (spawnTiles.Count == 0)
+            {
+                break;
+            }
+
+            // ランダムなタイルを選択する
+            int randomIndex = Random.Range(0, spawnTiles.Count);
+            HexTileObject targetTile = spawnTiles[randomIndex];
+
+            // 選択したタイルを候補から除外する
+            spawnTiles.RemoveAt(randomIndex);
+
+            // 敵を生成する
+            EnemyBase enemy = Instantiate(enemyPrefab, targetTile.transform.position, Quaternion.identity);
+
+            // デバッグ用の名前を設定する
+            enemy.name = $"Enemy_{i}";
+
+            // 敵の現在タイルを設定する
+            enemy.SetTile(targetTile.ID);
         }
-
-        if(spawnTileID < 0) return null;
-
-        // 敵を生成する
-        EnemyBase enemy = Instantiate(enemyPrefab);
-
-        // タイルへ配置する
-        enemy.SetTile(spawnTileID);
-
-        // 管理対象へ登録する
-        characterManager.Register(enemy);
-
-        return enemy;
     }
 }

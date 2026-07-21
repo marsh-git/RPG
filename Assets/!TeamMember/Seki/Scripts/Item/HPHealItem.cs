f98a04b7ecff681acfd30f04979fda11b2e77dd9
Ry-Seki <mcd2476094@stu.o-hara.ac.jp>
2026-07-21 15:00:22 +0900

    デバッグ用のアイテム及び、スクリプトの整理

--- Assets/!TeamMember/Seki/Scripts/Item/HPHealItem.cs
+++ Assets/!TeamMember/Seki/Scripts/Item/HPHealItem.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IItemEffect {
+public class HPHealItem : IItemEffect {
     /// <summary>
     /// アイテムの使用
     /// </summary>
     /// <param name="targetChara"></param>
     public void UseItem(CharacterBase targetChara) {
-
+        targetChara.Heal(20);
     }
 }
